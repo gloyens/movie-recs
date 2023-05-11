582a05d49bb475c58f7e904e0a71498286abcdd3
George <georgegloyens@gmail.com>
2023-05-11 11:11:16 +0100

    move prompt to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import { Container } from "./styles";
 import askOpenAI from "./api/generateAnswer";
 
 export default function Home() {
+  const prompt = "How do I make a waldorf salad?";
+
   return (
     <Container>
       <h1>Hello world</h1>
-      <p>{askOpenAI()}</p>
+      <p>{askOpenAI(prompt)}</p>
     </Container>
   );
 }

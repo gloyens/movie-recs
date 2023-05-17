7bda3abafd9e1709b93270e164673361c01b82cf
George <georgegloyens@gmail.com>
2023-05-17 10:32:00 +0100

    prettier

--- src/app/api/imdb.ts
+++ src/app/api/imdb.ts
@@ -8,7 +8,7 @@ async function searchIMDb(title: string): Promise<string | undefined> {
   const html = await response.text();
 
   const $ = load(html);
-  const firstResult = $('div.g').first().find('a').first().attr('href');
+  const firstResult = $("div.g").first().find("a").first().attr("href");
 
   return firstResult;
 }

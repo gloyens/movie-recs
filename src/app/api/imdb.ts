653e54828fbd4e08aeeb08afc3054ac0e35a7a81
George <georgegloyens@gmail.com>
2023-05-17 11:22:32 +0100

    prettier

--- src/app/api/imdb.ts
+++ src/app/api/imdb.ts
@@ -1,10 +1,8 @@
-import { load } from "cheerio";
-
 async function searchIMDb(title: string): Promise<string | undefined> {
   const url = `https://duckduckgo.com/?q=!ducky+${encodeURIComponent(
     title + " imdb"
   )}`;
-  
+
   return url;
 }
 

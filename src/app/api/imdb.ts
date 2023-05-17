6e24e517e5ffab9f47d6c60ee0e249378a5cd045
George <georgegloyens@gmail.com>
2023-05-17 10:31:15 +0100

    prettier

--- src/app/api/imdb.ts
+++ src/app/api/imdb.ts
@@ -1,16 +1,14 @@
 import { load } from "cheerio";
 
 async function searchIMDb(title: string): Promise<string | undefined> {
-  const url = `https://www.google.com/search?q=${encodeURIComponent(title + " imdb")}`;
+  const url = `https://www.google.com/search?q=${encodeURIComponent(
+    title + " imdb"
+  )}`;
   const response = await fetch(url);
   const html = await response.text();
-  
+
   const $ = load(html);
-  const firstResult = $('div.g')
-    .first()
-    .find('a')
-    .first()
-    .attr('href');
+  const firstResult = $('div.g').first().find('a').first().attr('href');
 
   return firstResult;
 }

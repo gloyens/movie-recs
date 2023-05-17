68dac3b0bb8cf037a304e82fcfb34b5599f0ead2
George <georgegloyens@gmail.com>
2023-05-17 11:21:18 +0100

    duckduckgo alternative

--- src/app/api/imdb.ts
+++ src/app/api/imdb.ts
@@ -1,23 +1,11 @@
-import nameToImdb from "name-to-imdb";
+import { load } from "cheerio";
 
-function convertTitle(input: string): { name: string, year: number } {
-  const regex = /^(.*?) \((\d+)\)$/;
-  const matches = input.match(regex);
-
-  if (!matches || matches.length !== 3) {
-    throw new Error('Invalid input format');
-  }
-
-  const name = matches[1];
-  const year = parseInt(matches[2]);
-
-  return { name, year };
-}
-
-async function searchIMDb(title: string) {
-  const titleObject = convertTitle(title);
-
-  console.log(nameToImdb(titleObject))
+async function searchIMDb(title: string): Promise<string | undefined> {
+  const url = `https://duckduckgo.com/?q=!ducky+${encodeURIComponent(
+    title + " imdb"
+  )}`;
+  
+  return url;
 }
 
 export default searchIMDb;
